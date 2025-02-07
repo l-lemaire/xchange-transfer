555b8674c17bc5128495917f576ace68ccd09d21
Loïc Lemaire <loic.j.lemaire@gmail.com>
2025-02-07 20:10:41 +0100

    Spring Initializer

---
+++ src/main/java/lu/llemaire/xchange_transfer/XchangeTransferApplication.java
@@ -0,0 +1,13 @@
+package lu.llemaire.xchange_transfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XchangeTransferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XchangeTransferApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lu/llemaire/xchange_transfer/XchangeTransferApplicationTests.java
@@ -0,0 +1,13 @@
+package lu.llemaire.xchange_transfer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XchangeTransferApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
